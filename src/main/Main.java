7b302e7e238d64c7f55fe9b28394c98c60e2010a
Angad <gurangadbatth@gmail.com>
2026-06-09 15:12:38 -0400

    Initialize Main with GameWindow Import

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import view.GameWindow;
+
+public class Main{
+    public static void main(String[] args) {
+        // Initialize the game window which starts the application
+        new GameWindow();
+    }
+
+}
