f376c43b3fbcbc5571090efb15b469158e7f0aea
Angad <gurangadbatth@gmail.com>
2026-06-09 08:40:08 -0400

    Constants class

---
+++ TheHeist/src/utils/Constants.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class Constants{
+    // Player physicals and dimentions
+    public static final int PLAYER_WIDTH = 32;
+    public static final int PLAYER_HEIGHT = 32;
+    public static final double Gravity = 0.5;
+    public static final double MAX_FALL_SPEED = 12.0;
+    public static final double JUMP_STRENGTH = -10.0;
+    public static final double RUN_SPEED = 5.0;
+
+    // Screen dimentions ( we will use in Renderer and GameWindow )
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 600;
+}
