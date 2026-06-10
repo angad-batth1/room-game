9aeda20c3002af8820a755a60be1b6d63086f492
Angad <gurangadbatth@gmail.com>
2026-06-09 22:10:31 -0400

    GameState Finished, implement stack in manager

--- src/controller/GameState.java
+++ src/controller/GameState.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.awt.Graphics2D;
 /*
 Let us implement a game state using stack, when the game boots up,
 we push() the main menu onto the stack. If they click play, we push()
@@ -9,6 +10,15 @@ resume, we pop() the pause menu off, and then the game continues.
 To do this, we must first implement gamestate, then we can implement gamestatemanager.
 */
 
-public class GameState {
+public abstract class GameState {
+    private GameStateManager gsm;
+
+    public GameState(GameStateManager gsm){
+        this.gsm = gsm;
+    }
+
+    // every screen must have a way to update its math and draw its graphics
+    public abstract void update();
+    public abstract void render(Graphics2D g2d);
     
 }
