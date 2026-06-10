ed361206cf549003065ebaa6264dc06081d6698a
Angad <gurangadbatth@gmail.com>
2026-06-09 22:02:34 -0400

    Game State setup, abstract class

---
+++ src/controller/GameState.java
@@ -0,0 +1,14 @@
+package controller;
+
+/*
+Let us implement a game state using stack, when the game boots up,
+we push() the main menu onto the stack. If they click play, we push()
+to level 1 state. If they hit escape, we push() a pause menu, if they
+resume, we pop() the pause menu off, and then the game continues.
+
+To do this, we must first implement gamestate, then we can implement gamestatemanager.
+*/
+
+public class GameState {
+    
+}
