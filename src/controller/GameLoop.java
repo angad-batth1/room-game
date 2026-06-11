29f3a4b439216771eb997a46134bc496acd92e87
Angad <gurangadbatth@gmail.com>
2026-06-11 08:51:31 -0400

    Changed Runnable to ActionListener, Base skeleton of game loop completed

--- src/controller/GameLoop.java
+++ src/controller/GameLoop.java
@@ -1,26 +1,35 @@
 package controller;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 import view.Renderer;
-import utils.Constants;
 
-public class Gameloop implements Runnable{
-    // private thread
-    // private running boolean
-
-    // private gamestatemanager
-    // private renderer
-
-    // public gameloop
-
-
-    //@override
-    // public void run
-    /*
-        Time per frame
-
-        Delta that accumulates frames then runs program
+public class Gameloop implements ActionListener{
     
-    
-    
-    */
+    private Timer timer;
+    private GameStateManager gsm;
+    private Renderer renderer;
+
+    public GameLoop(GamestateManager gsm, Renderer renderer){
+        this.gsm = gsm;
+        this.renderer = renderer;
+
+        // Calculate the delay
+
+        // Intialize the swing timer, where 'this' means it will call the actionperformed method
+
+    }
+
+    public void start(){
+        if(!timer.isRunning()){
+            timer.start();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        // update all the game math
+        // update the visual graphics of the screen.
+    }
 }
